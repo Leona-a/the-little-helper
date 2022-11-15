2fa2a00f4a4fd43c32c482f1ed4740b0c7dc7b3a
GaryE1am <grelam@uncg.edu>
2022-11-14 21:44:47 -0500

    adds admin files

--- api/src/main/java/com/tlh/api/TodoListController.java
+++ api/src/main/java/com/tlh/api/TodoListController.java
@@ -15,27 +15,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoListController {
     
     private final TodoListService todoListService;
-
+    private TodoListUserService todoListUserService;
+    
     @Autowired
     public TodoListController(TodoListService todoListService) {
         this.todoListService = todoListService;
     }
-
+    @Autowired
+    public void ToDoListUserController(TodoListUserService todoListUserService){
+        this.todoListUserService = todoListUserService;
+    }
+    
     @GetMapping(path = "{userToken}")
     public Iterable<TodoList> getTodoList(@PathVariable("userToken") String userToken) {
         return todoListService.getTodoList(userToken);
     }
-    
+    @GetMapping(path = "(userId)")
+    public Iterable<TodoListUser> getTodoListUser(@PathVariable("email") Integer userId){
+        return todoListUserService.getTodoListUser(userId);
+    }
     @GetMapping(path = "{userToken}/{listId}")
     public Optional<TodoList> getTodoListById(@PathVariable("listId") int id) {
         return todoListService.getTodoListById(id);
     }
-    
+    @GetMapping(path = "{userId}/{email}")
+    public Optional<TodoListUser> getTodoListUserById(@PathVariable("email") Integer id){
+        return todoListUserService.getTodoUserItemByUserId(id);
+    }
     @PostMapping
     public TodoList createList(@RequestBody TodoList list) {
         return todoListService.create(list);
     }
-    
+    /*
+    @PostMapping
+    public TodoListUser createUserList(@RequestBody TodoListUser user){
+        return todoListUserService.create(user);
+    }
+    */
     @DeleteMapping(path = "{listId}")
     public boolean deleteList(@PathVariable("listId") int id) {
         todoListService.delete(id);
